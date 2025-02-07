6075135b64c12d9396a76e3c0d35bf09cd3d86e9
S Suwannapaond <saharat.rus@gmail.com>
2025-02-07 11:06:37 +0700

    first commit

---
+++ main.ts
@@ -0,0 +1,44 @@
+let num_1 = parseInt(prompt("Enter a number") || "");
+let num_2 = parseInt(prompt("Enter a number") || "");
+let operation = prompt("Enter an operation (+, -, *, /):") || "";
+let result: number | null = null;
+
+while (true) {
+    switch (operation) {
+        case "+":
+            result = num_1 + num_2;
+            break;
+        case "-":
+            result = num_1 - num_2;
+            break;
+        case "*":
+            result = num_1 * num_2;
+            break;
+        case "/":
+            if (num_2 !== 0) {
+                result = num_1 / num_2;
+            } else {
+                console.log("Error: Division by zero");
+                result = null;
+            }
+            break;
+        default:
+            console.log("Invalid operation");
+            result = null;
+            break;
+    }
+
+    if (result !== null) {
+        console.log("Result:", result);
+
+        const cont = prompt("Do you want to continue? (y/n):") || "";
+        if (cont.toLowerCase() === "n") {
+            break;
+        } else {
+            num_1 = parseInt(prompt("Enter a number") || "");
+            num_2 = parseInt(prompt("Enter a number") || "");
+            operation = prompt("Enter an operation (+, -, *, /):") || "";
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# deno install 🚀
+```bash
+irm https://deno.land/install.ps1 | iex
+```
+# deno run
+
+```bash
+deno run main.ts
+```
+# deno create
+```bash
+F1 = deno init 
+OR
+terminal = deno run
+```
