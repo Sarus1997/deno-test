d03cede9df975667cdacd614b2defa913a387821
S Suwannapaond <saharat.rus@gmail.com>
2025-02-07 14:42:07 +0700

    update

--- index_03.ts
+++ index_03.ts
@@ -2,33 +2,41 @@ console.log("\nพีรามิด\n");
 
 for (let i = 0; i < 3; i++) {
   let line = '';
+
   for (let j = 0; j <= i; j++) {
     line += '*';
   }
+
   console.log(line);
 }
 
 
 for (let i = 3; i > 0; i--) {
   let line = "";
+
   for (let j = 0; j < 3 - i; j++) {
     line += " ";
   }
+
   for (let j = 0; j < i; j++) {
     line += "*";
   }
+
   console.log(line);
 }
 
 
 for (let i = 0; i < 3; i++) {
   let line = "";
+
   for (let j = 0; j < 3 - i; j++) {
     line += " ";
   }
+
   for (let j = 0; j < i; j++) {
     line += "*";
   }
+
   console.log(line);
 }
 
@@ -38,8 +46,10 @@ for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3 - i; j++) {
     line += " ";
   }
+
   for (let j = 0; j <= i; j++) {
     line += "* ";
   }
+
   console.log(line);
 }
